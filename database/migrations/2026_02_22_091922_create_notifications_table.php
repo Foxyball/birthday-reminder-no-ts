0ca53b1a9930f201f14b8d18681b828df79eea6b
Hristo Sabev <thefoxybg@gmail.com>
2026-04-04 15:46:23 +0300

    Reapply "fix primary key"
    This reverts commit 9faac69800244a9566103692d0884880d795c634.

--- database/migrations/2026_02_22_091922_create_notifications_table.php
+++ database/migrations/2026_02_22_091922_create_notifications_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('notifications', function (Blueprint $table) {
-            $table->uuid('id')->primary();
+            $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('title');
             $table->text('message')->nullable();
