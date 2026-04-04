d8d6d6f95233afe9931622aaae66e2630501220d
Foxyball <Foxyball@users.noreply.github.com>
2026-04-04 15:59:50 +0000

    PHP Linting (Pint)

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,28 +2,28 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Facades\Auth;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
     public function index()
     {
         $user = Auth::user();
-        
+
         $totalContacts = $user->contacts()->count();
-        
+
         $currentMonth = Carbon::now()->month;
         $upcomingBirthdays = $user->contacts()
             ->whereRaw('MONTH(birthday) = ?', [$currentMonth])
             ->count();
-        
+
         // Recent 5 contacts added
         $recentContacts = $user->contacts()
             ->orderBy('created_at', 'desc')
             ->take(5)
             ->get();
-        
+
         return view('index', [
             'totalContacts' => $totalContacts,
             'upcomingBirthdays' => $upcomingBirthdays,
