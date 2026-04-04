1b96cad61df5b50a102e241b137990f29e2b84c3
Hristo Sabev <thefoxybg@gmail.com>
2026-04-04 16:31:00 +0300

    change from sender

--- app/Mail/BirthdayReminderMail.php
+++ app/Mail/BirthdayReminderMail.php
@@ -29,7 +29,7 @@ class BirthdayReminderMail extends Mailable
     {
         return new Envelope(
             subject: __('messages.birthday_reminder_subject', ['count' => count($this->contacts)]),
-            from: config('mail.from.address'),
+            from: 'BalikG Reminder',
         );
     }
 
