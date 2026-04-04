3cc2fa863b35346266aadc8ed8b7f198bd8403ff
Hristo Sabev <thefoxybg@gmail.com>
2026-04-04 18:52:50 +0300

    update dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,10 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Auth;
+use Carbon\Carbon;
+
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('index');
+        $user = Auth::user();
+        
+        $totalContacts = $user->contacts()->count();
+        
+        $currentMonth = Carbon::now()->month;
+        $upcomingBirthdays = $user->contacts()
+            ->whereRaw('MONTH(birthday) = ?', [$currentMonth])
+            ->count();
+        
+        // Recent 5 contacts added
+        $recentContacts = $user->contacts()
+            ->orderBy('created_at', 'desc')
+            ->take(5)
+            ->get();
+        
+        return view('index', [
+            'totalContacts' => $totalContacts,
+            'upcomingBirthdays' => $upcomingBirthdays,
+            'recentContacts' => $recentContacts,
+        ]);
     }
 }
